5555e42db5e419525726f66f584cddfca987fbbd
Francois-Rene Rideau <tunes@google.com>
2015-07-18 10:07:57 +0200

    delta-position 0.1.0.1: update the project file.

--- delta-position/project.clj
+++ delta-position/project.clj
@@ -1,9 +1,9 @@
-(defproject leijure/delta-position "0.1.0"
+(defproject leijure/delta-position "0.1.0.1"
   :description "incremental position"
   ;; :url "TBD"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :source-paths ["src"]
   :test-paths ["test"]
-  :main leijure.delta-position)
+  :aot :all)
